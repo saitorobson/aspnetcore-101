e6ade425a3ab8ec289b8c88820c96b2007d089fc
Robson Saito <robson.saito@terra.com.br>
2020-10-21 00:59:25 -0300

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
